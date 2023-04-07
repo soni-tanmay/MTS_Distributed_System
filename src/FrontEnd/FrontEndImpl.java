db13863754379899181d6db5027b3dfaeb2bf75d
tanmaysoni <tanmay.soni.09@gmail.com>
2023-04-07 00:55:11 -0400

    added frontend interface and implementation

--- src/FrontEnd/FrontEndImpl.java
+++ src/FrontEnd/FrontEndImpl.java
@@ -1,4 +1,38 @@
 package FrontEnd;
 
-public class FrontEndImpl {
+public class FrontEndImpl implements  IFrontEnd{
+    @Override
+    public String addMovieSlots(String movieID, String movieName, int bookingCapacity) {
+        return null;
+    }
+
+    @Override
+    public String removeMovieSlots(String movieID, String movieName, boolean isClientCall) {
+        return null;
+    }
+
+    @Override
+    public String listMovieShowsAvailability(String movieName, boolean isClientCall) {
+        return null;
+    }
+
+    @Override
+    public String bookMovieTickets(String customerID, String movieID, String movieName, int numberOfTickets) {
+        return null;
+    }
+
+    @Override
+    public String getBookingSchedule(String customerID, boolean isClientCall) {
+        return null;
+    }
+
+    @Override
+    public String cancelMovieTickets(String customerID, String movieID, String movieName, int numberOfTickets) {
+        return null;
+    }
+
+    @Override
+    public String exchangeTickets(String customerID, String movieID, String old_movieName, String new_movieID, String new_movieName, int numberOfTickets) {
+        return null;
+    }
 }
--- src/FrontEnd/IFrontEnd.java
+++ src/FrontEnd/IFrontEnd.java
@@ -1,4 +1,12 @@
 package FrontEnd;
 
 public interface IFrontEnd {
+    String addMovieSlots (String movieID, String movieName, int bookingCapacity);
+    String removeMovieSlots (String movieID, String movieName, boolean isClientCall);
+    String listMovieShowsAvailability (String movieName, boolean isClientCall);
+    String bookMovieTickets (String customerID, String movieID, String movieName, int numberOfTickets);
+    String getBookingSchedule (String customerID, boolean isClientCall);
+    String cancelMovieTickets (String customerID, String movieID, String movieName, int numberOfTickets);
+    String exchangeTickets (String customerID, String movieID, String old_movieName, String new_movieID, String new_movieName, int numberOfTickets);
+
 }
