41048e7b17db4343d3570c55bfef09b81288c1e0
Apoorva Sharma <apoorvasharma@apoorvas-mbp.wireless.concordia.ca>
2023-04-08 03:04:57 -0400

    WIP: broadcast to RMs

--- src/Sequencer/Sequencer.java
+++ src/Sequencer/Sequencer.java
@@ -11,16 +11,16 @@ import java.net.UnknownHostException;
 import Utils.Constants;
 
 public class Sequencer {
-    // private InetAddress multicastAddr;
-    // private int multicastPort;
+    private InetAddress multicastAddr;
+    private int multicastPort;
     private DatagramSocket socket;
 
     private int sequenceNo;
 
-    public Sequencer() throws SocketException {
+    public Sequencer(InetAddress multicastAddr, int multicastPort) throws SocketException {
         this.socket = new DatagramSocket(7002);
-        // this.multicastAddr = multicastAddr;
-        // this.multicastPort = multicastPort;
+        this.multicastAddr = multicastAddr;
+        this.multicastPort = multicastPort;
     }
 
     private void listen() throws IOException {
@@ -45,7 +45,7 @@ public class Sequencer {
             System.out.println("seqData" + seqData);
 
             System.out.println("seqNo " + seqNo);
-            // byte[] message = seqData.getBytes();
+            byte[] message = seqData.getBytes();
             
             sendSeqNoToFE(dp, seqData);
         }
@@ -76,8 +76,35 @@ public class Sequencer {
             }
     }
 
+    private void broadcastToRm(byte[] data, int multicastPort) {
+        DatagramSocket socket1;
+                try {
+                    socket1 = new DatagramSocket();
+                } catch (SocketException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    socket1.setBroadcast(true);
+                } catch (SocketException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // System.out.println("MulticastAddress - "+multicastAddress);
+                System.out.println("MulticastPort - "+multicastPort);
+
+                DatagramPacket dp
+                        = new DatagramPacket(data, data.length, addr, multicastPort);
+                try {
+                    socket1.send(dp);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                socket1.close();
+
+    }
+
     public static void main(String[] args) throws IOException {
-        Sequencer seq = new Sequencer();
+        Sequencer seq = new Sequencer(InetAddress.getByName(Constants.RM1_IPAddress), Constants.RM1_Port);
         seq.listen();
     }
 }
