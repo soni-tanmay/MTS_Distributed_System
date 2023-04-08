7e89baf0d62f0cfc086142c5d9cefe7314338a62
tanmaysoni <tanmay.soni.09@gmail.com>
2023-04-07 20:18:03 -0400

    frontend send request

---
+++ src/Utils/Constants.java
@@ -0,0 +1,8 @@
+package Utils;
+
+public class Constants {
+    public static int FEPort = 7001;
+    public static String FE_IP = "172.31.69.41"; // Tanmay
+    public static int SQPort = 7002;
+    public static String SQ_IP = "172.30.39.158"; // Apoorva
+}
