07f9b1dac305ca332b6635e02cea331829f220a3
Apoorva Sharma <apoorvasharma@apoorvas-mbp.wireless.concordia.ca>
2023-04-07 20:19:54 -0400

    Adds Sequencer

--- src/Sequencer/Sequencer.java
+++ src/Sequencer/Sequencer.java
@@ -1,4 +1,63 @@
 package Sequencer;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+// import java.net.InetAddress;
+import java.net.SocketException;
+
 public class Sequencer {
+    // private InetAddress multicastAddr;
+    // private int multicastPort;
+    private DatagramSocket socket;
+
+    private int sequenceNo;
+
+    public Sequencer() throws SocketException {
+        this.socket = new DatagramSocket(7000);
+        // this.multicastAddr = multicastAddr;
+        // this.multicastPort = multicastPort;
+    }
+
+    private void listen() throws IOException {
+        byte[] bufferData = new byte[1024];
+        DatagramPacket dp = new DatagramPacket(bufferData, bufferData.length);
+
+        while(true) {
+            System.out.println("Sequencer listening to FE at port ");
+            socket.receive(dp);
+
+            // String sentenceData = new String(dp.getData(), 0, dp.getLength());
+            // String[] splitSentenceData  = sentenceData.split(";");
+
+            int seqNo = generateSequenceNo();
+
+            sendSeqNoToFE(dp, seqNo);
+
+
+
+        }
+    }
+
+    private int generateSequenceNo() {
+        return ++this.sequenceNo;
+    }
+
+    private void sendSeqNoToFE(DatagramPacket dp, int seqNo) {
+        InetAddress client = null;
+        try {
+            client = InetAddress.getLocalHost();
+        } catch (Exception e) {
+            // TODO: handle exception
+            throw new RuntimeException(e);
+        }
+        
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Sequencer seq = new Sequencer();
+        seq.listen();
+    }
 }
